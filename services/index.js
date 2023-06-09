df5e9a4bf4bb776cc3d2589ad557a7fbb128a6f1
Bhupendra Madhu <68414570+TheViz02@users.noreply.github.com>
2023-06-09 23:48:40 +0530

    Service & utility
    Updated to ES6 & service-utility based architecture in the API response

--- services/index.js
+++ services/index.js
@@ -1,2 +1,9 @@
-module.exports.loginService = require("./auth/login.service");
-module.exports.registerService = require("./auth/register.service");
+import * as loginService from "./auth/login.service.js";
+import * as registerService from "./auth/register.service.js";
+import * as indexService from "./index.service.js";
+
+export const services = {
+  loginService,
+  registerService,
+  indexService,
+};
