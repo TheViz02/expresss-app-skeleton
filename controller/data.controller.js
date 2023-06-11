8194bff42b45a1dc1f89c5e17381c09e490fc393
Bhupendra Madhu <68414570+TheViz02@users.noreply.github.com>
2023-06-11 12:47:48 +0530

    Save Data API
    API for saving Data in database, issue fix in service class calling.

--- controller/data.controller.js
+++ controller/data.controller.js
@@ -1,7 +1,16 @@
 import { services } from "../services/index.js";
+
 const { dataService } = services;
+const DataService = new dataService.DataService();
+
 export const getData = (req, res) => {
-  dataService.DataService.prototype.getData().then((result) => {
+  DataService.getData().then((result) => {
     res.status(200).send(result);
+  });
+};
+
+export const saveData = (req, res) => {
+  DataService.saveData(req.body).then((resp) => {
+    res.status(200).send(resp);
   });
 };
