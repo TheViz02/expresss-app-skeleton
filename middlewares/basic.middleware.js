67ebad437a8aeeaaf2af5757d6d8ac22e5be8385
Bhupendra Madhu <68414570+TheViz02@users.noreply.github.com>
2023-06-17 17:04:38 +0530

    Logging for the application

--- middlewares/basic.middleware.js
+++ middlewares/basic.middleware.js
@@ -1,16 +1,18 @@
 "use strict";
 import { errorResponse } from "../utils/apiResponse.js";
+import { log } from "../utils/logging.js";
 
 export const basicCheck = (req, res, next) => {
   let s = true;
+  log.info(s);
   if (s) next();
   else res.redirect("/another");
 };
 
 export const errorHandling = (err, req, res, next) => {
   if (err) {
+    // console.log(err);
     res.locals.message = err.message;
-
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
